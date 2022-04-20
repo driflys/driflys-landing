115a545c0a7de4f091e8507013b89aab75982217
TharindaPrabhath <tharindahp@gmail.com>
2022-04-20 15:10:38 +0530

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,15 @@ import Page from "../components/Page";
 import { common } from "../constants";
 import LandingLayout from "../layouts/landing.layout";
 
+// images
+import Design from "../public/features/Design.png";
+import StatusTrack from "../public/features/StatusTrack.png";
+import Verify from "../public/features/Verify.png";
+import Share from "../public/features/Share.png";
+import OnlineView from "../public/features/OnlineView.png";
+import OnlineViewHeading from "../public/features/OnlineViewHeading.svg";
+import BrandPromote from "../public/features/BrandPromote.png";
+
 const Home = () => {
   return (
     <Page
@@ -17,8 +26,8 @@ const Home = () => {
         </>
       }
     >
-      <section id="hero" className="relative h-screen">
-        <div className="container flex flex-col justify-center pt-14 lg:pt-28 gap-12 md:items-center md:text-center z-10">
+      <section id="hero" className="relative h-5/6 md:h-screen">
+        <div className="container flex flex-col justify-center py-14 lg:pt-28 gap-12 md:items-center md:text-center z-10">
           <div>
             <h1 className="font-Exo2 text-4xl md:text-6xl font-bold max-w-2xl">
               Create design & send{" "}
@@ -71,6 +80,132 @@ const Home = () => {
         </div>
         <div className="absolute -bottom-40 -left-20 w-96 h-96 rounded-full bg-blue-700 filter blur-3xl opacity-30 -z-10"></div>
         <div className="absolute -top-40 -right-20 w-96 h-96 rounded-full bg-slate-800 filter blur-3xl opacity-30 -z-10"></div>
+      </section>
+
+      <section id="features" className="bg-white">
+        <div className="container py-20 flex flex-col gap-40 lg:px-20">
+          {/* Feature 1 */}
+          <div className="flex flex-col justify-center items-center gap-12 lg:flex-row lg:justify-between lg:gap-12">
+            <div className="w-3/4 max-w-lg lg:3/4">
+              <Image
+                src={Design}
+                alt="Design Certificates"
+                layout="responsive"
+              />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 lg:items-start">
+              <h1 className="font-bold text-center text-4xl lg:text-6xl lg:text-left">
+                <span className="text-blue-700">Design</span> certificates the
+                way you want
+              </h1>
+              <p className="text-gray-500 text-center lg:text-lg lg:text-left md:w-3/4">
+                You can customize the look & feel of your certificates by using
+                our drag n drop builder{" "}
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 2 */}
+          <div className="flex flex-col justify-center items-center gap-12 lg:flex-row-reverse lg:justify-between lg:gap-12">
+            <div className="w-2/5 max-w-xs md:w-1/5">
+              <Image src={StatusTrack} alt="Status Track" layout="responsive" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 lg:items-start">
+              <h1 className="font-bold text-center text-4xl lg:text-6xl lg:text-left">
+                Deliver with <span className="text-blue-700">Confident</span>
+              </h1>
+              <p className="text-gray-500 text-center lg:text-lg lg:text-left md:w-3/4">
+                With the help of realtime status tracking, you as the issuer can
+                track the status of certificate emails
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 3 */}
+          <div className="flex flex-col justify-center items-center gap-12 lg:flex-row lg:justify-between lg:gap-12">
+            <div className="w-3/4 max-w-xs lg:3/4">
+              <Image src={Verify} alt="Verify" layout="responsive" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 lg:items-start">
+              <h1 className="font-bold text-center text-4xl lg:text-6xl lg:text-left">
+                <span className="text-blue-700">Verify</span> the validity with
+                ease
+              </h1>
+              <p className="text-gray-500 text-center lg:text-lg lg:text-left md:w-3/4">
+                Receivers can verify the validity of their certificates using
+                unique certificate id
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 4 */}
+          <div className="flex flex-col justify-center items-center gap-12 lg:flex-row-reverse lg:justify-between lg:gap-12">
+            <div className="w-3/4 max-w-xs lg:3/4">
+              <Image src={Share} alt="Share" layout="responsive" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 lg:items-start">
+              <h1 className="font-bold text-center text-4xl lg:text-6xl lg:text-left">
+                <span className="text-blue-700">Show</span> the world what you
+                achieved
+              </h1>
+              <p className="text-gray-500 text-center lg:text-lg lg:text-left md:w-3/4">
+                Receivers can verify the validity of their certificates using
+                unique certificate id
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 5 */}
+          <div className="flex flex-col justify-center items-center gap-4 lg:gap-12">
+            <div className="w-4/5 md:w-3/5">
+              <Image
+                src={OnlineView}
+                alt="Online Certificate View"
+                layout="responsive"
+              />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-8 lg:flex-row-reverse lg:gap-20">
+              <div className="w-4/5 md:w-2/5">
+                <Image
+                  src={OnlineViewHeading}
+                  alt="View Anywhere Anytime"
+                  layout="responsive"
+                />
+              </div>
+              <div className="lg:h-80 lg:w-px lg:bg-gray-500 lg:opacity-60"></div>
+              <p className="text-xl text-gray-500 text-center lg:text-right md:w-1/2">
+                We host all the issued certificates via Driflys platform to
+                assure that the receivers can view/interact with their
+                certificates 24/7
+              </p>
+            </div>
+          </div>
+
+          <div className="container flex justify-center items-center content-center">
+            <div className="py-20 px-4 bg-gradient-to-r from-blue-700 via-blue-900 to-blue-900 rounded-t-3xl flex flex-col justify-center items-center gap-12">
+              <div className="w-2/5 md:1/5">
+                <Image
+                  src={BrandPromote}
+                  alt="Brand Promote"
+                  layout="responsive"
+                />
+              </div>
+              <div className="flex flex-col justify-center items-center gap-8">
+                <h1 className="text-6xl text-white font-semibold text-center">
+                  Promote your brand
+                </h1>
+                <p className="text-lg text-gray-400 text-center md:w-2/3">{`Essential brand details such as logo, name & social media links can be displayed in the certificate view page. 
+We believe that this feature makes perfect impact both
+the popularity of brand and receiver’s trustworthiness upon your brand`}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section id="contactUs" className="bg-white">
