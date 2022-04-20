81a0aee315f28adc8f421711ffab83e559c112a6
TharindaPrabhath <tharindahp@gmail.com>
2022-04-20 15:40:28 +0530

    -

--- components/footers/landing.footer.tsx
+++ components/footers/landing.footer.tsx
@@ -7,6 +7,10 @@ import Link from "next/link";
 import dayjs from "dayjs";
 import { common } from "../../constants";
 
+// icons
+import MailIcon from "@heroicons/react/solid/MailIcon";
+import LocationMarkerIcon from "@heroicons/react/solid/LocationMarkerIcon";
+
 function LandingFooter() {
   return (
     <footer className="bg-white">
@@ -43,30 +47,11 @@ function LandingFooter() {
         <div className="col-start-1 row-start-3 col-span-2 md:col-start-3 md:row-start-2 md:col-span-1 flex md:justify-end">
           <ul className="flex flex-col gap-2">
             <li className="flex flex-row items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg>
+              <MailIcon className="w-5 h-5 text-gray-800" />
               <p className="text-gray-800">driflys@gmail.com</p>
             </li>
             <li className="flex flex-row items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <LocationMarkerIcon className="w-5 h-5 text-gray-800" />
               <p className="text-gray-800">Matale, Sri Lanka</p>
             </li>
           </ul>
--- components/headers/landing.header.tsx
+++ components/headers/landing.header.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 
 import { common } from "../../constants";
 
+import MenuIcon from "@heroicons/react/outline/MenuIcon";
+
 function LandingHeader() {
   return (
     <header className="container z-50">
@@ -27,22 +29,7 @@ function LandingHeader() {
           <li className="cursor-pointer hover:text-black">Features</li>
           <li className="cursor-pointer hover:text-black">Contact Us</li>
         </ul>
-        <div className="md:hidden cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </div>
+        <MenuIcon className="w-6 h-6 text-black md:hidden cursor-pointer" />
       </nav>
       <div className="absolute left-0 top-16 bg-gray-300 w-full h-px"></div>
     </header>
