5efaf01496cac05b21337723d0494c8949b54f41
driflys <driflys@gmail.com>
2022-05-20 19:58:23 +0530

    -

--- components/footers/simple.footer.tsx
+++ components/footers/simple.footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function SimpleFooter() {
   return (
     <footer className="p-5 text-center text-gray-500">
-      <p>2022 All Rights Reserved ©️ Driflys Inc.</p>
+      <p>2022 All Rights Reserved ©️ Driflys</p>
     </footer>
   );
 }
