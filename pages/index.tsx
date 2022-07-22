9f1c13a0815f87322da29b91b4a4506af38f7674
driflys <driflys@gmail.com>
2022-07-22 15:10:25 +0530

    move info card into components

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { MainLayout } from "../layouts"
 
 // components
 import Page from "../components/Page"
+import InfoCard from "../components/InfoCard"
 
 // hero icons
 import ChevronRightIcon from "@heroicons/react/outline/ChevronRightIcon"
@@ -163,19 +164,3 @@ const Home = () => {
 Home.layout = MainLayout
 
 export default Home
-
-interface InfoCardProps {
-  title: string
-  description: string
-  icon: React.ReactNode
-}
-
-const InfoCard = ({ title, description, icon }: InfoCardProps) => {
-  return (
-    <div className="bg-white drop-shadow-xl py-8 px-4 rounded-lg">
-      <div className="mx-auto w-fit p-3 bg-gray-100 rounded-full">{icon}</div>
-      <h1 className="mt-2 text-lg text-center font-semibold">{title}</h1>
-      <p className="mt-2 mx-auto text-md text-center max-w-xs">{description}</p>
-    </div>
-  )
-}
