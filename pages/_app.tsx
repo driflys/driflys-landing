f161fead41ffc896e25d75894d18809a9d0103c9
driflys <driflys@gmail.com>
2022-08-04 19:14:03 +0530

    set up google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,53 @@
 // react
-import { useEffect } from "react";
+import { useEffect } from "react"
 
 // next
-import { useRouter } from "next/router";
+import Script from "next/script"
+import { useRouter } from "next/router"
 
-import "tailwindcss/tailwind.css";
+import "tailwindcss/tailwind.css"
 
 // react-toastify
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-import { pageView } from "../utils/gtag";
+import { pageView } from "../utils/gtag"
+import { env } from "../constants/env"
 
 function MyApp({ Component, pageProps }: any) {
-  const Layout = Component.layout || (({ children }: any) => <>{children}</>);
-  const router = useRouter();
+  const Layout = Component.layout || (({ children }: any) => <>{children}</>)
+  const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
-      pageView(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
+      pageView(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${env.GA_ID}`} />
+      <Script strategy="lazyOnload">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${env.GA_ID}', {
+                page_path: window.location.pathname,
+              });
+          `}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
       <ToastContainer position="bottom-right" />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
