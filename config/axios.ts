c634d8c2ea8711195ffe17519b178fb12dbc89d3
driflys <driflys@gmail.com>
2022-08-06 18:10:01 +0530

    fix invalid prod api base url

--- config/axios.ts
+++ config/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosStatic } from "axios"
 
-const API_BASE_URL_PROD = "https://ap.driflys.com/v1"
+const API_BASE_URL_PROD = "https://api.driflys.com/v1"
 const API_BASE_URL_DEV = "http://localhost:5000/v1"
 // NOTE: Set withCredentials: true to send & receive cookies
 //       Otherwise the server has set the cookies correctly, the client won't be set those cookies
