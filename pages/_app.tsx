dfb887f67964e9b85f3efc761181d3147fa2f3f0
driflys <driflys@gmail.com>
2022-08-23 00:41:50 +0530

    replace ga4 direct integration with google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,20 +12,18 @@ import { env } from "../constants/env"
 function MyApp({ Component, pageProps }: any) {
   const Layout = Component.layout || (({ children }: any) => <>{children}</>)
 
+  console.log("Google", env.GOOGLE_TAG_MANAGER_ID)
+
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script id="" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${env.GA_ID}`} />
-      <Script id="" strategy="lazyOnload">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${env.GA_ID}', {
-                page_path: window.location.pathname,
-              });
-          `}
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${env.GOOGLE_TAG_MANAGER_ID}');`}
       </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
